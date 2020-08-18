1040697e9db075ca603b98025f7a89b70878f5bc
npysklyw <npysklyw@uwo.ca>
2020-08-17 22:02:50 -0400

    Fixing site

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, redirect, url_for, session
+from flask import Flask, render_template, session, redirect, url_for, session,request
 from flask_wtf import FlaskForm
 from wtforms import TextField,SubmitField
 from wtforms.validators import NumberRange
@@ -9,15 +9,11 @@ import tensorflow.keras
 import joblib
 import scaler
 
-def return_prediction(model,scaler,sample_json):
+def return_prediction(model,scaler,image):
     #function takes image, converts to numpy array preprocesses it, then predicts what case is
 
-    local_link_to_image = sample_json['linktoimg']
-
-    image = [[local_link_to_image]]
-    
     #we need to create our own scaler for user data preprocess
-    imagedata = scaler.scale(local_link_to_image)
+    imagedata = scaler.scale(image)
     
 
     classes = np.array(['mild-demented', 'moderate-demented','non-demented','very-mild-demented'])
@@ -28,8 +24,7 @@ def return_prediction(model,scaler,sample_json):
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecretkey'
 
-model = tensorflow.keras.models.load_model("model_one.h5")
-   
+model =  tensorflow.keras.models.load_model('model_one.h5')   
 class ALZForm(FlaskForm):
     link_img = TextField('Link to Image')
 
@@ -48,14 +43,19 @@ def index():
 
     return render_template('index.html', form=form)
 
-@app.route('/prediction')
+@app.route('/success', methods = ['POST'])  
+def success():  
+    if request.method == 'POST':  
+        f = request.files['file']  
+        f.save(f.filename)  
+        return render_template("success.html", name = f.filename)  
+        
+@app.route('/prediction', methods = ['POST','GET'])
 def prediction():
 
-    content = {}
+ 
 
-    content['linktoimg'] = str(session['link_img'])
-
-    results = return_prediction(model=model,scaler=scaler,sample_json=content)
+    results = return_prediction(model=model,scaler=scaler,image=request.files['file'] )
 
     
     return render_template('prediction.html',results=results)
--- scaler.py
+++ scaler.py
@@ -2,15 +2,23 @@ from tensorflow.keras.preprocessing import image
 from PIL import Image
 from flask import request
 import numpy as np
+from PIL import Image
+import cv2
+import io
 
 # example of converting an image with the Keras API
 from keras.preprocessing.image import load_img
 from keras.preprocessing.image import img_to_array
+from keras.preprocessing.image import smart_resize
 
-def scale(imageurl):
-    aimage = load_img(imageurl,target_size=(178,208))
-    n = img_to_array(aimage)
-    n = [n/255]
+def scale(image):
+    
+    photo = image
+    photo.save('img.jpg')
+
+    a = load_img('img.jpg',target_size=(178,208,3))
+    a = img_to_array(a)
+    n = [a/255]
     return np.array(n)
 
 
