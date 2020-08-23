3db213c4fbfacbc339e135bcffa6b488de808fa5
npysklyw <npysklyw@uwo.ca>
2020-08-23 11:04:44 -0400

    Changes to add in percetage bars

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 from flask import Flask, render_template, session, redirect, url_for, session,request
-from flask_wtf import FlaskForm
-from wtforms import TextField,SubmitField
-from wtforms.validators import NumberRange
 
 import keras.models
 import numpy as np  
@@ -11,12 +8,9 @@ import boto3
 import os
 
 
-s3 = boto3.resource('s3',
-       aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'] ,
-      aws_secret_access_key= os.environ['AWS_SECRET_ACCESS_KEY'] )
+s3 = boto3.resource('s3',aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],aws_secret_access_key= os.environ['AWS_SECRET_ACCESS_KEY'] )
 
-s3.Object(os.environ['S3_BUCKET_NAME'], 'model_one.h5').download_file(
-   f'model_one.h5') # Python 3.6+
+s3.Object(os.environ['S3_BUCKET_NAME'], 'model_one.h5').download_file( f'model_one.h5') # Python 3.6+
 
 
 def return_prediction(model,scaler,image):
@@ -24,7 +18,6 @@ def return_prediction(model,scaler,image):
 
     #we need to create our own scaler for user data preprocess
     imagedata = scaler.scale(image)
-    
 
     classes = np.array(['Mild Dementia', 'Moderate Dementia','No Dementia','Very Mild Dementia'])
     guess = model.predict_on_batch(imagedata)
@@ -36,40 +29,30 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecretkey'
 
 model =  keras.models.load_model('model_one.h5')   
-class ALZForm(FlaskForm):
-    link_img = TextField('Link to Image')
-
-    submit = SubmitField('Analyze')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
 
-    form = ALZForm()
-
-    if form.validate_on_submit():
-
-        session['link_img'] = form.link_img.data
-
-        return redirect(url_for("prediction"))
-
-    return render_template('index.html', form=form)
-
-@app.route('/success', methods = ['POST'])  
-def success():  
-    if request.method == 'POST':  
-        f = request.files['file']  
-        f.save(f.filename)  
-        return render_template("success.html", name = f.filename)  
+    return render_template('index.html')
         
 @app.route('/prediction', methods = ['POST','GET'])
 def prediction():
 
- 
-
     results, percents = return_prediction(model=model,scaler=scaler,image=request.files['file'] )
 
-    
-    return render_template('prediction.html',results=results,milddem= (100*percents[0,1]),moddem=(100*percents[0,1]),nondem=(100*percents[0,2]),vmd=(100*percents[0,3]))
+    vm = np.around((100*percents[0,3]),3)
+    milddem = np.around((100*percents[0,1]),3)
+
+    nondem = np.around((100*percents[0,2]),3)
+    moddem = np.around((100*percents[0,1]),3)
+
+    mildemw = str(np.max([milddem,1])) + "%"
+    moddemw = str(np.max([moddem,1])) + "%"
+    nondemw = str(np.max([nondem,1])) + "%"
+
+    vmdw = str(np.max([vm,1])) + "%"
+ 
+    return render_template('prediction.html',results=results,milddem= milddem,moddem=moddem,nondem=nondem,vmd=vm, mildemw=mildemw,moddemw=moddemw,nondemw=nondemw,vmdw=vmdw)
 
 if __name__ == '__main__':
     app.run(debug=True)
